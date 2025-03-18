317cc7ffee10fbef70be731ecee21a0b6a1cd109
Sandikxya <s_l224@student.usc.edu.au>
2025-03-18 13:02:03 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    serialLineReceived = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    commandParts = serialLineReceived.split(" ")
+    command = commandParts[0]
+    if (command == "heart") {
+        basic.showIcon(IconNames.Heart)
+    } else if (command == "diamond") {
+        basic.showIcon(IconNames.Diamond)
+    } else if (command == "square") {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (command == "sensitivity") {
+        if (commandParts.length > 1) {
+            sensitivity = parseFloat(commandParts[1])
+        }
+    } else if (command == "heartbeat") {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(500)
+    } else {
+        basic.showString(command)
+    }
+})
+let command = ""
+let commandParts: string[] = []
+let serialLineReceived = ""
+let sensitivity = 0
+serial.setBaudRate(BaudRate.BaudRate115200)
+sensitivity = 255
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.forever(function () {
+    led.setBrightness(input.soundLevel() * (sensitivity / 255))
+})
